0afab67f33fb2159a8d6fc7bfc473503bdafef80
Justin <justin.gebert@student.htw-berlin.de>
2022-12-15 13:05:50 +0100

    small changes to main app

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -2,15 +2,48 @@ import java.util.ArrayList;
 
 public class User {
 
+    private int id;
     private String email;
     private String username;
     //maybe shoudlnt be here rather in db
     private String password;
 
-    public User(String email,String username,String password){
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
         this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
         this.password = password;
+    }
+
+    public User(String email, String username, String password){
+        setEmail(email);
+        setUsername(username);
+        setPassword(password);
     }
     public ArrayList<Project> projects = new ArrayList();
 
